2cb4ba5221b651aa5bd0c6ac750adb9e8af7bd80
Caike De Oliveira Domingues <caike.dom@gmail.com>
2025-01-30 17:53:13 -0300

    aula 4 da imersão front-end da alura - parte 1

---
+++ script.js
@@ -0,0 +1,26 @@
+
+/*Em javascript, DOM significa Document Object Model (Modelo de Objeto
+de Documento). Ele é uma interface que permite que o javascript interaja
+com o conteúdo de páginas web. */
+
+/*Imagine o  Dom como uma arvore:
+
+A raiz da arvore: O documento HTML em si
+
+Os galhos: Os elementos HTML (como paragráfos, imagens, titulos, etc)
+
+As folhas: O texto dentro dos elementos.
+*/
+
+/*O DOM representa essa estrutura de forma que o JavaScript possa acessar e modificar cada parte dela.  */
+
+
+const searchInput = document.getElementById('search-input');
+
+/*Eventos: interações com o usuário ou funcionalidades da página ,
+basicamente são ações que o sistema realiza.*/
+
+document.addEventListener('input', function() {
+
+    const =.
+})
